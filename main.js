24aab7d43584cc79e481e05d246b6652ef997fb2
Aiden <draakelias28@gmail.com>
2025-06-23 15:32:21 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+function startTime() {
+    const today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('txt').innerHTML = h + ":" + m + ":" + s;
+    setTimeout(startTime, 1000);
+}
+
+function checkTime(i) {
+    if (i < 10) { i = "0" + i; }  // add zero in front of numbers < 10
+    return i;
+}
+
+document.addEventListener('contextmenu', event => event.preventDefault());
+
+var elem = document.getElementById("htmls");
+
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) { /* Safari */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE11 */
+    elem.msRequestFullscreen();
+  }
+}
